d1e217170fa3406748120162bb12821bad8fac57
mjbza <mahdijaberzadehansari@yahoo.co.uk>
2019-09-11 19:11:45 +0200

    New application #4 has been added. First app in Web IDE

---
+++ Samples/app4/README.md
@@ -0,0 +1,3 @@
+# This application had been created with Web IDE template applications. 
+
+![Sample Output](./Output.JPG)
---
+++ Samples/app4/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/mjzsoft/demo/ui5/app4/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
