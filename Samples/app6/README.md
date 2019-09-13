5436bc2341d488a59670897f9f819813faf90b0f
mjbza <mahdijaberzadehansari@yahoo.co.uk>
2019-09-13 09:27:47 +0200

    App 6 added

---
+++ Samples/app6/README.md
@@ -0,0 +1,3 @@
+# This application shows how to add new controls or elements to your view by using Layout editor.
+
+![Sample Output](./Output.JPG)
---
+++ Samples/app6/webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.mjzsoft.demo.ui5.app6.controller.Main", {
+		onInit: function () {
+
+		}
+	});
+});
